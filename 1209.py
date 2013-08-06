ec7b007aef6cc667c70d7ec52a2953828e8bf930
nat <contact@tuannat.me>
2013-08-06 18:00:31 +0900

    1209

--- 1209.py
+++ 1209.py
@@ -1,4 +1,14 @@
 from math import sqrt
 
-def check(k):
+def check(x):
+	x = (sqrt(8*x -7) - 1)/2
+	return "1" if x == int(x) else "0"
+	
+n = int(raw_input())
+a = []
+for i in range(n):
+	x = int(raw_input())
+	a.append(x)
+	
+print " ".join([check(x_) for x_ in a])
 	
